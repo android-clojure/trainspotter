8e36de82cb7a3441960638e0f783b6555ba4161c
Tuukka Koistinen <tvkoisti@gmail.com>
2017-01-10 19:43:14 +0200

    experimental api functions

--- trainspotter/src/clojure/org/trainspotter/rata.clj
+++ trainspotter/src/clojure/org/trainspotter/rata.clj
@@ -1,8 +1,20 @@
 (ns org.trainspotter.rata
-  (:require [clj-http.lite.client :as client]))
+  (:require [clj-http.lite.client :as client]
+            [cheshire.core :as json]))
 
 (def ^:const API_URL "https://rata.digitraffic.fi/api/v1/")
 
 (def ^:const TRAIN_ID 8461)
 
-(client/get (str API_URL "live-trains/" TRAIN_ID) {:accept :json})
+(defn api-query [& more]
+  (let [response (client/get (str API_URL (first more)) {:accept :json})
+        status (:status response)]
+    (assert (= status 200) (str "Error:" status))
+    (json/parse-string (:body response))
+    ))
+
+(defn get-stations []
+  (api-query "metadata/stations"))
+
+(defn get-train [train-id]
+  (api-query "live-trains/" train-id))
