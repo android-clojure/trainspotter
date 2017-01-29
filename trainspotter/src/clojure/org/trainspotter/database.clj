a87d7c26cccd18d7ca346bec2688dbf474416d21
Tuukka Koistinen <tvkoisti@gmail.com>
2017-01-29 16:17:21 +0200

    add robolectric tests for database

--- trainspotter/src/clojure/org/trainspotter/database.clj
+++ trainspotter/src/clojure/org/trainspotter/database.clj
@@ -1,6 +1,8 @@
-(ns org.trainspotter.rata.database
+(ns org.trainspotter.database
   (:require [neko.data.sqlite :as db]
-            [org.trainspotter.log :as log]))
+            [org.trainspotter.log :as log])
+  (:import android.database.sqlite.SQLiteDatabase
+          neko.data.sqlite.TaggedDatabase))
 
 (def trainspotter-schema
   (db/make-schema
@@ -9,17 +11,21 @@
     :tables {:trains
              {:columns
               {:_id "integer primary key"
-               :train-id "integer not null default '0'"}}}))
+               :train_id "integer not null"}}}))
 
 (def get-db-helper
   (memoize
     (fn [] (db/create-helper trainspotter-schema))))
 
-(defn- trainspotter-db [] (db/get-database (get-db-helper) :write))
+(defn trainspotter-db [] (db/get-database (get-db-helper) :write))
+
+(defn wipe []
+  (log/i "Wiping database!")
+  (.delete ^SQLiteDatabase (.db ^TaggedDatabase (trainspotter-db)) "trains" "1" nil))
 
 (defn get-id [entry-seq]
   (:_id entry-seq))
 
 (defn add-train [train-id]
-  (log/d "add train to watch: " train-id)
-  (db/insert (trainspotter-db) :trians {:train-id train-id}))
+  (log/d "add train to watch:" train-id)
+  (db/insert (trainspotter-db) :trains {:train_id train-id}))
