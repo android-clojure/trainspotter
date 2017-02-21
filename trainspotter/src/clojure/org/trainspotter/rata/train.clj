eb29d78db1b9279facbd2c71a86a33f746399f37
Tuukka Koistinen <tvkoisti@gmail.com>
2017-02-21 20:24:59 +0200

    refactor train api

--- trainspotter/src/clojure/org/trainspotter/rata/train.clj
+++ trainspotter/src/clojure/org/trainspotter/rata/train.clj
@@ -9,12 +9,50 @@
 (defn running? [train]
   (= (:runningCurrently train) true))
 
-(defn get-stops [time-table station]
-  (filter #(= (:stationShortCode %) station) time-table))
+(defn get-time-table [train]
+  (:timeTableRows train))
+
+; (defn get-stops [time-table station]
+;   (filter #(= (:stationShortCode %) station) time-table))
+
+(defn first-station [train]
+  {:post (= (:type %) "DEPARTURE")}
+  (first (get-time-table train)))
+
+(defn last-station [train]
+  {:post (= (:type %) "ARRIVAL")}
+  (last (get-time-table train)))
+
+(defn get-stop [train station stop-type]
+  {:pre (keyword? stop-type)}
+  (let [time-table (get-time-table train)
+        stype (case stop-type
+                :departure "DEPARTURE"
+                :arrival "ARRIVAL"
+                "DEPARTURE")]
+    (first
+      (filter #(and (= (:stationShortCode %) station)
+                    (= (:type %) stype)) time-table))))
 
 (defn get-cancelled-stops [stops]
   (seq (filter :cancelled stops)))
 
+; (defn late? [train station]
+;   (< 0 (:differenceInMinutes
+;          (get-stop
+;            train
+;            station
+;            :departure))))
+
+(defn get-late-info [train station]
+  (let [late-info (select-keys
+                    (get-stop
+                      train
+                      station
+                      :departure)
+                    [:differenceInMinutes :causes])]
+    (assoc late-info :late (< 0 (:differenceInMinutes late-info)))))
+
 (defn cancelled? [train station]
   (or (:cancelled train) ;; <- TODO is this needed?
-       (not (empty? (get-cancelled-stops (get-stops (:timeTableRows train) station))))))
+      (:cancelled (get-stop train station :departure))))
